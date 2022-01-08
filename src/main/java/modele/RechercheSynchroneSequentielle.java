09722d633f2deeade0409ed048b0eda09ddb027f
Raphaël Painter <raphapainter@wanadoo.fr>
2022-01-08 17:28:19 +0100

    adding empty search algorithms to implement

---
+++ src/main/java/modele/RechercheSynchroneSequentielle.java
@@ -0,0 +1,30 @@
+package modele;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.ws.rs.client.Client;
+
+import infrastructure.jaxrs.HyperLien;
+
+public class RechercheSynchroneSequentielle extends RechercheSynchroneAbstraite{
+
+	@Override
+	public Optional<HyperLien<Livre>> chercher(Livre l, List<HyperLien<Bibliotheque>> bibliotheques, Client client) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public NomAlgorithme nom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Optional<HyperLien<Livre>> rechercheSync(HyperLien<Bibliotheque> h, Livre l, Client client) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
