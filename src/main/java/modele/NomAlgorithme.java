e79acdd5af57b1893ff951bf409454d455694c33
Simon SASSI <12765875+maxerbox@users.noreply.github.com>
2022-01-09 18:00:14 +0100

    fix: null value of algorithm name

--- src/main/java/modele/NomAlgorithme.java
+++ src/main/java/modele/NomAlgorithme.java
@@ -2,7 +2,12 @@ package modele;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
-@XmlRootElement(name="algo")
+@XmlRootElement(name = "algo")
 public interface NomAlgorithme {
+
+	public static NomAlgorithme fromString(String titre) {
+		return new ImplemNomAlgorithme(titre);
+	}
+
 	String getNom();
 }
